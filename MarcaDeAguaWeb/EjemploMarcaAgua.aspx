c474048f8a539ea8483ea1731fbf16856c2b28a5
miguel0507 <magj2208@gmail.com>
2021-10-19 10:46:43 -0500

    Agregar archivos de proyecto.

---
+++ MarcaDeAguaWeb/EjemploMarcaAgua.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EjemploMarcaAgua.aspx.cs" Inherits="MarcaDeAguaWeb.EjemploMarcaAgua" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Creacion de Marca de Agua en Imagenes</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <asp:Button ID="BtnUpload" Text="Subir Archivo" runat="server" OnClick="Upload" />
+        
+        <div>
+            <asp:Image ID="ImagenMarcaAgua" Width="650" Height="500" runat="server" Visible="false" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ MarcaDeAguaWeb/EjemploMarcaAgua.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MarcaDeAguaWeb
+{
+    public partial class EjemploMarcaAgua : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Upload(object sender, EventArgs e)
+        {
+            string watermarkText = "Prueba - Marca de Agua";
+
+            //Se obtiener el nombre del archivo
+            string fileName = Path.GetFileName(FileUpload1.PostedFile.FileName);
+
+            //Se lee el archivo en un Bitmap
+            using(Bitmap bmp = new Bitmap(FileUpload1.PostedFile.InputStream, false))
+            {
+                using(Graphics grp = Graphics.FromImage(bmp))
+                {
+                    //Se setea el color del texto de la marca de agua
+                    Brush brush = new SolidBrush(Color.Black);
+
+                    //Se setea la fuente y su tamaño
+                    Font font = new Font("Courier New", 30, FontStyle.Bold, GraphicsUnit.Pixel);
+
+                    //Determina el tamaño del texto de la marca de agua
+                    SizeF textSize = new SizeF();
+                    textSize = grp.MeasureString(watermarkText, font);
+
+                    //Se asigna la posicion del texto en la imagen y se inserta
+                    //Se pueden agregar distintas posiciones para el texto
+                    Point position = new Point((bmp.Width - ((int)textSize.Width)), (bmp.Height - ((int)textSize.Height + 10)));
+                    grp.DrawString(watermarkText, font, brush, position);
+
+                    using(MemoryStream memoryStream= new MemoryStream())
+                    {
+                        //Se guarda la imagen con marca de agua en un memory stream
+                        bmp.Save(memoryStream, ImageFormat.Jpeg);
+                        memoryStream.Position = 0;
+
+                        //Se convierte a base64 para insertar en etiqueta de imagen y se muestre en navegador
+                        byte[] bytes;
+                        bytes = memoryStream.ToArray();
+                        string base64 = Convert.ToBase64String(bytes);
+                        ImagenMarcaAgua.Visible = true;
+                        ImagenMarcaAgua.ImageUrl = string.Concat("data:image/jpeg;base64,",
+                            base64);
+                    }
+                }
+            }
+        }
+    }
+}
